fe4062e33784735f5c91da4635cc6e4fe3bf74a1
Harrison-Lane-1/22 <ycazam@my.lanecc.edu>
2019-02-20 16:13:24 -0800

    Almost done with Memory part of Lab 5, just need Shuffle

---
+++ MemoryFilenameInTagDone/CardClasses/Card.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardClasses
+{
+    public class Card
+    {
+        // Card class variables
+        private int suit; // suit ranges from 1 to 4
+        private int value; // value ranges from 1 to 13
+
+        private string[] suits = { "????", "clubs", "diamonds", "hearts", "spades" };
+        private string[] values = { "????", "ace", "2", "3", "4", "5", "6", "7", "8", "9", "ten", "jack", "queen", "king" };
+
+        // Card class constructors
+        // Default constructor
+        public Card()
+        {
+            value = 1;
+            suit = 1;
+        }
+
+        // Overloaded constructor
+        public Card(int v, int s)
+        {
+            value = v;
+            suit = s;
+        }
+
+        // Getters and Setters
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public int Suit
+        {
+            get
+            {
+                return suit;
+            }
+            set
+            {
+                suit = value;
+            }
+        }
+
+        // File Name Property
+        public string FileName
+        {
+            get
+            {
+                return "card" + values[value].Substring(0, 1) + 
+                    suits[suit].Substring(0, 1) + ".jpg";
+            }
+        }
+        // Matching Value Method
+        public bool HasMatchingValue(Card otherCard)
+        {
+            if (otherCard.Value == this.value)
+                return true;
+            else
+                return false;
+        }
+
+        // Methods that set the image to the given PictureBox to the proper FileName
+        // Card Face
+        public void Show(PictureBox p)
+        {
+            p.Image = Image.FromFile(System.Environment.CurrentDirectory +
+                "\\Cards\\" + FileName);
+        }
+        // Card Back
+        public void ShowBack(PictureBox p)
+        {
+            p.Image = Image.FromFile(System.Environment.CurrentDirectory +
+                "\\Cards\\black_back.jpg");
+        }
+
+        // ToString Method
+        public override string ToString()
+        {
+            return values[value] + " of " + suits[suit];
+        }
+
+        // CardIsSuit Methods
+        public bool IsHeart()
+        {
+            if (suit == 3)
+                return true;
+            else
+                return false;
+        }
+
+        // CardIsFace Methods
+        public bool IsFaceCard()
+        {
+            if (value >= 11 && value <= 13)
+                return true;
+            else
+                return false;
+        }
+
+        public bool IsQueen()
+        {
+            if (value == 12)
+                return true;
+            else
+                return false;
+        }
+
+        // CardIsColor Methods
+        public bool IsBlack()
+        {
+            if (suit == 1 || suit == 4)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ MemoryFilenameInTagDone/CardTests/Program.cs
@@ -0,0 +1,121 @@
+﻿using CardClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CardTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCardConstructors();
+
+            Card aS = new Card(1, 4);
+            Card qH = new Card(12, 3);
+
+            TestProperties();
+            Console.WriteLine();
+
+            TestHasMatchingValue();
+            Console.WriteLine();
+
+            TestCardIsMethods(aS);
+            Console.WriteLine();
+
+            TestCardIsMethods(qH);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+        // Test Card Constructors
+        static void TestCardConstructors()
+        {
+            Card card1 = new Card();
+            Card card2 = new Card(12, 3);
+
+            Console.WriteLine("Testing Constructors");
+            Console.WriteLine("Default constructor. Expecting default values. " + card1.ToString() + ".");
+            Console.WriteLine("Overloaded constructor. Expecting queen of hearts. " + card2.ToString() + ".");
+            Console.WriteLine();
+        }
+
+        // Test Properties
+        static void TestProperties()
+        {
+            Console.WriteLine("Testing Properties");
+            Card jS = new Card(11, 4);
+            string file1 = jS.FileName;
+            Console.WriteLine("Testing FileName: expecting cardjs.jpg");
+            Console.WriteLine(file1);
+            Console.WriteLine();
+        }
+
+        static void TestHasMatchingValue()
+        {
+            Console.WriteLine("Testing HasMatchingValue");
+            Card aS = new Card(1, 4);
+            Card qH = new Card(12, 3);
+            Card qC = new Card(12, 1);
+
+            Console.WriteLine("Expecting The values do not match.");
+            if (aS.HasMatchingValue(qH))
+                Console.WriteLine("The values match.");
+            else
+                Console.WriteLine("The values do not match.");
+
+            Console.WriteLine("Expecting The values match.");
+            if (qC.HasMatchingValue(qH))
+                Console.WriteLine("The values match.");
+            else
+                Console.WriteLine("The values do not match.");
+
+        }
+
+        // Test Card ToString Method
+        static void TestCardToString(Card card)
+        {
+            Console.WriteLine("Testing toString Method");
+            Console.WriteLine("The card is " + card.ToString() + ".");
+            Console.WriteLine();
+        }
+
+        // Test Card IsMethods
+        static void TestCardIsMethods(Card card)
+        {
+            Console.WriteLine("Testing Is Methods");
+            Console.WriteLine("The card is " + card.ToString() + ".");
+            Console.WriteLine();
+
+            if (card.IsHeart())
+                Console.WriteLine("The card is a heart.");
+            else
+                Console.WriteLine("The card is not a heart.");
+            Console.WriteLine();
+
+            if (card.IsFaceCard())
+                Console.WriteLine("The card is a face card");
+            else
+                Console.WriteLine("The card is not a face card.");
+            Console.WriteLine("");
+
+            if (card.IsQueen())
+                Console.WriteLine("The card is a queen.");
+            else
+                Console.WriteLine("The card is not a queen.");
+            Console.WriteLine();
+
+            if (card.IsBlack())
+                Console.WriteLine("The card is black.");
+            else
+                Console.WriteLine("The card is not black.");
+        }
+        
+    }
+}
